66f80d243e0ed9a3663c0e63aaa094a430f4b436
Marie Koscianski-Ducharlet <115166678+MarieKosDuc@users.noreply.github.com>
2023-03-08 11:30:09 +0100

    Try: implement a post route

--- index.js
+++ index.js
@@ -12,26 +12,35 @@ app.use(
   })
 );
 
+// API "homepage"
 app.get("/", (req, res) => {
   res.send(
     'Toutes les définitions : <a href="./alldefinitions">/alldefinitions</a> <br> Une définition au hasard : <a href="./definition">/definition</a> <br> Une définition avec son ID : /definition/:id'
   );
 });
 
+// route to get all definitions
 app.get("/alldefinitions", (req, res) => {
   res.status(200).json(definitions);
 });
 
+// route to get a random definition
 app.get("/definition", (req, res) => {
-  // To do : get a random definition
   const randomId = definitions[Math.floor(Math.random() * definitions.length)];
   res.status(200).json(randomId);
 });
 
+// route to get a definition by id
 app.get("/definition/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const parking = definitions.find((definitions) => definitions.id === id);
+  const definitions = definitions.find((definitions) => definitions.id === id);
   res.status(200).json(parking);
+});
+
+// route to create a new definition
+app.post("/definitions/", (req, res) => {
+  definitions.push(req.body);
+  res.status(200).json(definitions);
 });
 
 app.listen(port, () => console.log("Serveur à l'écoute"));
